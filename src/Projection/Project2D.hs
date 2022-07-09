65d3ada34fd8cbe3c3ac4a414e1f6e79c119a197
Anthony Suárez <neolight1010@gmail.com>
2022-07-09 09:17:18 -0500

    Make Project2D generic for Floating numbers.

--- src/Projection/Project2D.hs
+++ src/Projection/Project2D.hs
@@ -8,16 +8,17 @@ module Projection.Project2D
 where
 
 import Data.Angle (Angle, tanAngle)
+import Numeric ()
 
-data Camera2D = Camera2D
-  { angle :: Angle Float,
-    w :: Float
+data Camera2D a = Camera2D
+  { angle :: Angle a,
+    w :: a
   }
 
-type Point2D = (Float, Float)
+type Point2D a = (a, a)
 
 -- |Projects a 2D point to 1D.
-p2D :: Camera2D -> Point2D -> Float
+p2D :: Floating a => Camera2D a -> Point2D a -> a
 p2D cam (x', z') = dividend / divisor
   where
     dividend = x' * camW * tanAngle camAngle
@@ -30,18 +31,18 @@ p2D cam (x', z') = dividend / divisor
 
 type CameraX = Camera2D
 
-type PointXZ = Point2D
+type PointXZ a = Point2D a
 
 -- |Projects a 2D point to 1D on the X axis. Alias for p2D.
-px :: CameraX -> PointXZ -> Float
+px :: Floating a => CameraX a -> PointXZ a -> a
 px = p2D
 
 -- Y axis aliases
 
 type CameraY = Camera2D
 
-type PointYZ = Point2D
+type PointYZ a = Point2D a
 
 -- |Projects a 2D point to 1D on the Y axis. Alias for p2D.
-py :: CameraY -> PointYZ -> Float
+py :: Floating a => CameraY a -> PointYZ a -> a
 py = p2D
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,11 @@
 import Data.Angle (angleFromDegrees)
-import Projection.Project2D (Camera2D (..), Point2D, px, py, p2D)
+import Projection.Project2D (Camera2D (..), Point2D, p2D, px, py)
 import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
   describe "Projection on 2D" $ do
-    axisProjectionTest p2D 
+    axisProjectionTest p2D
 
   describe "Projection on x" $ do
     axisProjectionTest px
@@ -13,16 +13,23 @@ main = hspec $ do
   describe "Projection on y" $ do
     axisProjectionTest py
 
-axisProjectionTest :: (Camera2D -> Point2D -> Float) -> SpecWith ()
+axisProjectionTest :: (Camera2D Float -> Point2D Float -> Float) -> SpecWith ()
 axisProjectionTest proj = do
   it "returns 0 if x is 0" $ do
     let camera = Camera2D {angle = angleFromDegrees 30, w = 100}
 
-    proj camera (0, 30) `shouldBe` 0
+    proj camera (0, 30) `shouldApproximate` 0
 
   it "returns the value on the boundaries of the camera" $ do
     let cameraW = 1
     let farW = cameraW + 1
     let camera = Camera2D {angle = angleFromDegrees 45, w = cameraW}
 
-    px camera (farW, 1) `shouldBe` cameraW
+    px camera (farW, 1) `shouldApproximate` cameraW
+
+shouldApproximate :: (Fractional a, Ord a, Show a) => a -> a -> Expectation
+shouldApproximate a b = do
+  a `shouldSatisfy` (> b - allowedError)
+  a `shouldSatisfy` (< b + allowedError)
+  where
+    allowedError = 0.00005
