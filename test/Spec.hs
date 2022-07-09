751d1c7d091ce2712560d11b1de35a6a6588bac5
Anthony Suárez <neolight1010@gmail.com>
2022-07-08 23:28:13 -0500

    Refactor Projection tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,14 +5,18 @@ import Test.Hspec
 main :: IO ()
 main = hspec $ do
   describe "Projection on x" $ do
-    it "returns 0 if x is 0" $ do
-      let camera = Camera {angle = angleFromDegrees 30, w = 100}
+    axisProjectionTest px
 
-      px camera (0, 30) `shouldBe` 0
+axisProjectionTest :: (Camera -> (Float, Float) -> Float) -> SpecWith ()
+axisProjectionTest proj = do
+  it "returns 0 if x is 0" $ do
+    let camera = Camera {angle = angleFromDegrees 30, w = 100}
 
-    it "returns the value on the boundaries of the camera" $ do
-      let cameraW = 1
-      let farW = cameraW + 1
-      let camera = Camera {angle = angleFromDegrees 45, w = cameraW}
+    proj camera (0, 30) `shouldBe` 0
 
-      px camera (farW, 1) `shouldBe` cameraW
+  it "returns the value on the boundaries of the camera" $ do
+    let cameraW = 1
+    let farW = cameraW + 1
+    let camera = Camera {angle = angleFromDegrees 45, w = cameraW}
+
+    px camera (farW, 1) `shouldBe` cameraW
