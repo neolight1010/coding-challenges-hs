0f3d727485b2d7a856631bf55b86f7d1bc006f14
Anthony Suárez <neolight1010@gmail.com>
2022-08-28 12:00:19 -0500

    Define Snake data types

--- src/Snake.hs
+++ src/Snake.hs
@@ -1,11 +1,13 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+
 module Snake
   ( snakeIO,
   )
 where
 
-import Graphics.Gloss.Data.ViewPort (ViewPort)
-import Graphics.Gloss (Picture, simulate, Display (InWindow),)
+import Graphics.Gloss (Display (InWindow), Picture, simulate)
 import qualified Graphics.Gloss.Data.Color as Color
+import Graphics.Gloss.Data.ViewPort (ViewPort)
 
 width :: Int
 width = 800
@@ -13,14 +15,38 @@ width = 800
 height :: Int
 height = 600
 
-data SnakeGame = SnakeGame {
-}
+type Coord = (Int, Int)
+
+type BodyUnit = Coord
+
+data SnakeGame = SnakeGame
+  { snake :: Snake,
+    food :: [Food]
+  }
+
+data Snake = Snake
+  { body :: [BodyUnit],
+    direction :: Direction
+  }
+
+data Direction = Up | Down | Left | Right
+
+newtype Food = Food
+  { pos :: Coord
+  }
 
 snakeIO :: IO ()
-snakeIO = simulate (InWindow "Star Field" (width, height) (20, 20)) Color.black 60 SnakeGame renderSnakeGame updateSnakeGame
+snakeIO = simulate (InWindow "Star Field" (width, height) (20, 20)) Color.black 60 newSnakeGame renderSnakeGame updateSnakeGame
 
 renderSnakeGame :: SnakeGame -> Picture
 renderSnakeGame = undefined
 
 updateSnakeGame :: ViewPort -> Float -> SnakeGame -> SnakeGame
 updateSnakeGame = undefined
+
+newSnakeGame :: SnakeGame
+newSnakeGame =
+  SnakeGame
+    { snake = Snake {body = [(0, 0)], direction = Snake.Right},
+      food = []
+    }
